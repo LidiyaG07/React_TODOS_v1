c1e70cf92a93afa3aabef84eae1d07dd394226fc
LidiyaG07 <Gankoval.a.0108@mail.ru>
2024-01-06 00:08:52 +0300

    added code in app.js

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from './components/header';
+import ListItems from './components/listItems';
+import Form from './components/form';
 
 export default function App() {
+  const [listItems, setListOfItems] = useState([
+    {text: 'Test 1', key: '1'},
+    {text: 'Test 2', key: '2'},
+    {text: 'Test 3', key: '3'},
+    {text: 'Test 4', key: '4'},
+  ])
+
+  const addHandler = (text) => {
+    setListOfItems((list) => {
+      return[
+        {text: text, key: Math.random().toString(36).substring(7)},
+        ...list
+      ]
+    })
+  }
+
+  const deleteHandler = (key) => {
+    setListOfItems((list) => {
+      return list.filter(listItems => listItems.key != key )
+    });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View >
+      <Header />
+      <Form addHandler={addHandler} />
+      <View>
+        <FlatList data={setListOfItems} renderItem={({item}) => (
+          <ListItems el={item} deleteHandler={deleteHandler}/>
+        )} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
